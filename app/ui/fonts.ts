31e0c3d7c4e488660d4fe4da50fe6202b32911b9
Kathiravaan Subramanian <k.subramanian@globallogic.com>
2024-12-13 15:30:45 -0500

    initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin']});
+
+export const lusitana = Inter(
+{
+    weight: ['100'],
+    subsets: ['latin']
+});
